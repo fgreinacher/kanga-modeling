141405d24058f0f32e5bf43beb9f4c7f45a2a9f8
FlorianGreinacher <none@none>
2012-03-19 19:49:06 +0000

    - Added master solution, core projects and tests - Added NuGet package configuration - Added references Moq and NUnit to test projects
    --HG--
    extra : convert_revision : svn%3A37308e9d-ab76-48fb-8b0d-99fc395a6d69%4015001

---
+++ Main/Source/KangaModeling/AssemblyInfo.Common.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KangaModeling community")]
+[assembly: AssemblyProduct("KangaModeling")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Main/Source/KangaModeling/KangaModeling.Renderer/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KangaModeling.Renderer")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("151981fa-7249-4fa9-90b3-90449e6097cb")]
