ad35bba0b7296fa041387743a169782f045dfec4
StefanHanke <none@none>
2012-03-22 22:01:49 +0000

    cmdline tool now actually parses args and generates output
    --HG--
    extra : convert_revision : svn%3A37308e9d-ab76-48fb-8b0d-99fc395a6d69%4015077

--- Main/Source/KangaModeling/CommandLineRunner/CommandLineRunner.cs
+++ Main/Source/KangaModeling/CommandLineRunner/CommandLineRunner.cs
@@ -5,6 +5,11 @@ using System.Text;
 using KangaModeling.Compiler.SequenceDiagrams;
 using CommandLine;
 using System.Drawing.Imaging;
+using System.Drawing;
+using KangaModeling.Graphics.GdiPlus;
+using KangaModeling.Graphics.Theming;
+using KangaModeling.Layouter.SequenceDiagrams;
+using KangaModeling.Graphics.Renderables;
  
 
 namespace CommandLineRunner
@@ -16,22 +21,78 @@ namespace CommandLineRunner
     public sealed class CommandLineRunner
     {
 
-        public static void Main(String[] args)
+        public static int Main(String[] args)
         {
             Options opts = new Options();
             if (!new CommandLineParser().ParseArguments(args, opts))
             {
                 System.Console.Error.WriteLine(opts.GetHelp());
-                return;
+                return -1;
             }
             new CommandLineRunner().run(opts);
+            return 0;
         }
 
         internal void run(Options opts)
         {
             SequenceDiagram sd = Parser.ParseString(opts.Model);
+            Bitmap bm = genBitmap(sd);
 
-            // TODO make an image and put somewhere?!
+            ImageFormat format = ImageFormat.Png;
+            switch (opts.Format.ToLowerInvariant())
+            {
+                case "png": format = ImageFormat.Png; break;
+                case "bmp": format = ImageFormat.Bmp; break;
+                case "jpeg": format = ImageFormat.Jpeg; break;
+                default:
+                    throw new ArgumentException("unknown format: " + opts.Format);
+            }
+
+            bm.Save(opts.FileName, format);
+            bm.Dispose();
+        }
+
+        /// <summary>
+        /// intermediate; needs to be hidden!
+        /// </summary>
+        /// <param name="sd"></param>
+        /// <returns></returns>
+        private Bitmap genBitmap(SequenceDiagram sd)
+        {
+            ITheme theme = new SimpleTheme();
+
+            using(var measureBitmap = new Bitmap(1, 1))
+            using (var measureGraphics = System.Drawing.Graphics.FromImage(measureBitmap))
+            {
+                var measurerFactory = new GdiPlusMeasurerFactory(measureGraphics);
+                var measurer = measurerFactory.CreateMeasurer(theme);
+
+                var layoutEngine = new LayoutEngine(measurer);
+                var layoutResult = layoutEngine.PerformLayout(sd);
+
+                var renderBitmap = new Bitmap(
+                    (int)Math.Ceiling(layoutResult.Size.Width + 1),
+                    (int)Math.Ceiling(layoutResult.Size.Height + 1));
+
+                using (var renderGraphics = System.Drawing.Graphics.FromImage(renderBitmap))
+                {
+                    renderGraphics.Clear(Color.White);
+
+                    var rendererFactory = new GdiPlusRendererFactory(renderGraphics);
+                    var renderer = rendererFactory.CreateRenderer(theme);
+
+                    foreach (var renderable in layoutResult.Renderables)
+                    {
+                        var renderableText = renderable as RenderableText;
+                        if (renderableText != null)
+                        {
+                            renderer.RenderText(renderableText);
+                        }
+                    }
+                }
+
+                return renderBitmap;
+            }
         }
 
     }
