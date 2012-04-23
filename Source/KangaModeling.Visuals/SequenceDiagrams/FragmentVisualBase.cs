3eff012ad8c2e4c7b1ac56f59966e464acac58db
FlorianGreinacher <post@floriangreinacher.de>
2012-04-23 22:46:38 +0200

    First implementation steps to draw fragment operands appropriately

--- Source/KangaModeling.Visuals/SequenceDiagrams/FragmentVisualBase.cs
+++ Source/KangaModeling.Visuals/SequenceDiagrams/FragmentVisualBase.cs
@@ -5,92 +5,100 @@ using KangaModeling.Graphics.Primitives;
 
 namespace KangaModeling.Visuals.SequenceDiagrams
 {
-    internal class FragmentVisualBase : Visual
-    {
-        public const float FramePadding = 5;
-        private readonly IArea m_Area;
-        private readonly ICombinedFragment m_Fragment;
-        private readonly GridLayout m_GridLayout;
-        private Size m_HeaderSize;
-        private Padding m_InnerPadding;
+	internal class FragmentVisualBase : Visual
+	{
+		public const float FramePadding = 5;
+		private readonly IArea m_Area;
+		private readonly ICombinedFragment m_Fragment;
+		private readonly GridLayout m_GridLayout;
+		private Size m_HeaderSize;
+		private Padding m_InnerPadding;
 
-        public FragmentVisualBase(ICombinedFragment fragment, GridLayout gridLayout)
-        {
-            m_Fragment = fragment;
-            m_GridLayout = gridLayout;
-            m_Area = m_Fragment.GetArea();
-            Initialize();
-        }
+		public FragmentVisualBase(ICombinedFragment fragment, GridLayout gridLayout)
+		{
+			m_Fragment = fragment;
+			m_GridLayout = gridLayout;
+			m_Area = m_Fragment.GetArea();
+			Initialize();
+		}
 
-        protected Row TopRow { get; set; }
-        protected Row BottomRow { get; set; }
-        protected Column LeftColumn { get; set; }
-        protected Column RightColumn { get; set; }
+		protected Row TopRow { get; set; }
+		protected Row BottomRow { get; set; }
+		protected Column LeftColumn { get; set; }
+		protected Column RightColumn { get; set; }
 
-        protected IArea Area
-        {
-            get { return m_Area; }
-        }
+		protected IArea Area
+		{
+			get { return m_Area; }
+		}
 
-        private void Initialize()
-        {
-            foreach (IOperand operand in m_Fragment.Operands)
-            {
-                if (!operand.Signals.Any())
-                {
-                    continue;
-                }
+		private void Initialize()
+		{
+			m_InnerPadding =
+				new Padding(m_Area.LeftDepth(), m_Area.RightDepth(), m_Area.TopDepth(), m_Area.BottomDepth()) *
+				FramePadding;
 
-                int topRowIndex = operand.Signals.Select(signal => signal.RowIndex).Min();
-                Row topRow = m_GridLayout.Rows[topRowIndex];
-                AddChild(new OperandVisual(operand, topRow, m_GridLayout));
-            }
-        }
+			foreach (IOperand operand in m_Fragment.Operands)
+			{
+				if (!operand.Signals.Any())
+				{
+					continue;
+				}
 
-        protected internal override void LayoutCore(IGraphicContext graphicContext)
-        {
-            base.LayoutCore(graphicContext);
+				int topRowIndex = operand.Signals.Select(signal => signal.RowIndex).Min();
+				Row topRow = m_GridLayout.Rows[topRowIndex];
 
-            m_HeaderSize = graphicContext.MeasureText(m_Fragment.Title);
+				int leftColumnIndex = operand.Signals.Select(signal => signal.GetArea().Left).Min();
+				Column leftColumn = m_GridLayout.Columns[leftColumnIndex];
 
-            m_InnerPadding =
-                new Padding(m_Area.LeftDepth(), m_Area.RightDepth(), m_Area.TopDepth(), m_Area.BottomDepth())*
-                FramePadding;
+				int rightColumnIndex = operand.Signals.Select(signal => signal.GetArea().Right).Max();
+				Column rightColumn = m_GridLayout.Columns[rightColumnIndex];
 
-            TopRow.TopGap.Allocate(m_HeaderSize.Height + 8 + m_InnerPadding.Top);
-            BottomRow.BottomGap.Allocate(m_InnerPadding.Bottom);
+				AddChild(new OperandVisual(operand, topRow, leftColumn, rightColumn, m_GridLayout, m_InnerPadding));
+			}
+		}
 
-            LeftColumn.Allocate(m_InnerPadding.Left);
-            RightColumn.Allocate(m_InnerPadding.Right);
-        }
+		protected internal override void LayoutCore(IGraphicContext graphicContext)
+		{
+			base.LayoutCore(graphicContext);
 
-        protected override void DrawCore(IGraphicContext graphicContext)
-        {
-            float yStart = TopRow.TopGap.Bottom - m_HeaderSize.Height - 8;
-            float yEnd = BottomRow.BottomGap.Bottom + m_InnerPadding.Bottom;
+			m_HeaderSize = graphicContext.MeasureText(m_Fragment.Title);
 
-            float xStart = LeftColumn.Body.Left - m_InnerPadding.Left;
-            float xEnd = RightColumn.Body.Right + m_InnerPadding.Right;
 
-            DrawInternal(xEnd, xStart, yEnd, yStart, graphicContext);
-            base.DrawCore(graphicContext);
-        }
+			TopRow.TopGap.Allocate(m_HeaderSize.Height + 8 + m_InnerPadding.Top);
+			BottomRow.BottomGap.Allocate(m_InnerPadding.Bottom);
 
-        protected void DrawInternal(float xEnd, float xStart, float yEnd, float yStart, IGraphicContext graphicContext)
-        {
-            Size = new Size(xEnd - xStart, yEnd - yStart);
+			LeftColumn.Allocate(m_InnerPadding.Left);
+			RightColumn.Allocate(m_InnerPadding.Right);
+		}
 
-            var textLocation = new Point(xStart + 4, yStart + 4);
-            graphicContext.DrawText(m_Fragment.Title, HorizontalAlignment.Center, VerticalAlignment.Middle, textLocation,
-                                    m_HeaderSize);
-            graphicContext.DrawRectangle(new Point(xStart, yStart), Size);
+		protected override void DrawCore(IGraphicContext graphicContext)
+		{
+			float yStart = TopRow.TopGap.Bottom - m_HeaderSize.Height - 8;
+			float yEnd = BottomRow.BottomGap.Bottom + m_InnerPadding.Bottom;
 
-            var textFramePoint1 = new Point(xStart, yStart + m_HeaderSize.Height + 5);
-            var textFramePoint2 = new Point(xStart + m_HeaderSize.Width, yStart + m_HeaderSize.Height + 5);
-            var textFramePoint3 = new Point(xStart + m_HeaderSize.Width + 16, yStart);
+			float xStart = LeftColumn.Body.Left - m_InnerPadding.Left;
+			float xEnd = RightColumn.Body.Right + m_InnerPadding.Right;
 
-            graphicContext.DrawLine(textFramePoint1, textFramePoint2, 1);
-            graphicContext.DrawLine(textFramePoint2, textFramePoint3, 1);
-        }
-    }
+			DrawInternal(xEnd, xStart, yEnd, yStart, graphicContext);
+			base.DrawCore(graphicContext);
+		}
+
+		protected void DrawInternal(float xEnd, float xStart, float yEnd, float yStart, IGraphicContext graphicContext)
+		{
+			Size = new Size(xEnd - xStart, yEnd - yStart);
+
+			var textLocation = new Point(xStart + 4, yStart + 4);
+			graphicContext.DrawText(m_Fragment.Title, HorizontalAlignment.Center, VerticalAlignment.Bottom, textLocation,
+									m_HeaderSize);
+			graphicContext.DrawRectangle(new Point(xStart, yStart), Size);
+
+			var textFramePoint1 = new Point(xStart, yStart + m_HeaderSize.Height + 5);
+			var textFramePoint2 = new Point(xStart + m_HeaderSize.Width, yStart + m_HeaderSize.Height + 5);
+			var textFramePoint3 = new Point(xStart + m_HeaderSize.Width + 16, yStart);
+
+			graphicContext.DrawLine(textFramePoint1, textFramePoint2, 1);
+			graphicContext.DrawLine(textFramePoint2, textFramePoint3, 1);
+		}
+	}
 }
--- Source/KangaModeling.Visuals/SequenceDiagrams/OperandVisual.cs
+++ Source/KangaModeling.Visuals/SequenceDiagrams/OperandVisual.cs
@@ -8,14 +8,20 @@ namespace KangaModeling.Visuals.SequenceDiagrams
     {
         private readonly GridLayout m_GridLayout;
         private readonly IOperand m_Operand;
-        private readonly Row m_TopRow;
+		private readonly Row m_TopRow;
+		private readonly Column m_RightColumn;
+		private readonly Column m_LeftColumn;
         private Size m_TextSize;
+		private readonly Padding m_OuterPadding;
 
-        public OperandVisual(IOperand operand, Row topRow, GridLayout gridLayout)
+        public OperandVisual(IOperand operand, Row topRow, Column leftColumn, Column rightColumn, GridLayout gridLayout, Padding outerPadding)
         {
             m_Operand = operand;
             m_TopRow = topRow;
+			m_LeftColumn = leftColumn;
+			m_RightColumn = rightColumn;
             m_GridLayout = gridLayout;
+			m_OuterPadding = outerPadding;
             Initialize();
         }
 
@@ -41,11 +47,11 @@ namespace KangaModeling.Visuals.SequenceDiagrams
             float yText = m_TopRow.Top;
             float yLine = yText + m_TextSize.Height;
 
-            //float xStart = m_LeftColumn.Left;
-            //float xEnd = m_RightColumn.Right;
-            //graphicContext.DrawDashedLine(new Point(xStart, yLine), new Point(xEnd, yLine), 1);
+			float xStart = m_LeftColumn.Body.Left - m_OuterPadding.Left;
+			float xEnd = m_RightColumn.Body.Right + m_OuterPadding.Right;
 
-            //graphicContext.DrawText(m_Operand.GuardExpression, HorizontalAlignment.Center, VerticalAlignment.Middle, new Point(xStart, yText), m_TextSize);
+			graphicContext.DrawDashedLine(new Point(xStart, yLine), new Point(xEnd, yLine), 1);
+			graphicContext.DrawText(m_Operand.GuardExpression, HorizontalAlignment.Center, VerticalAlignment.Top, new Point(xStart, yText), m_TextSize);
 
             base.DrawCore(graphicContext);
         }
