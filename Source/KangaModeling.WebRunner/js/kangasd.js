776e5378017c0be588fcf2b52bb20ad410fbf09a
gmamaladze <gmamaladze@gmail.com>
2012-05-02 11:01:17 +0200

    Implemented support for including diagrams in web sites. Updated http://kangamodeling.org/ web content.

---
+++ Source/KangaModeling.WebRunner/js/kangasd.js
@@ -0,0 +1,29 @@
+﻿function drawDiagrams() {
+    var sourceElements = getSourceElements();
+    for (i = 0; i < sourceElements.length; i++) {
+        convertCodeToDiagram(sourceElements[i]);
+    }
+}
+
+function convertCodeToDiagram(element) {
+    var code = element.childNodes[0].data;
+    element.innerHTML = '';
+    var img = document.createElement('img');
+    img.setAttribute('src', 'http://kangamodeling.org/SequenceDiagram.aspx?code=' + encodeURI(code));
+    img.setAttribute('alt', code);
+    element.appendChild(img);
+}
+
+function getSourceElements() {
+    var classname = 'kanga.sd';
+    var result = [];
+    var els = document.getElementsByTagName('pre');
+    for (var i = 0, j = els.length; i < j; i++) {
+        if (els[i].className == classname) {
+            result.push(els[i]);
+        }
+    }
+    return result;
+}
+
+drawDiagrams();
