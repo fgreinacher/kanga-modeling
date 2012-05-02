f6a1f748262c67fed32f4548800ebbeae58939c9
gmamaladze <gmamaladze@gmail.com>
2012-05-02 18:55:59 +0200

    Improved start page layout. Closing memory stream.

--- Source/KangaModeling.WebRunner/SequenceDiagram.aspx.cs
+++ Source/KangaModeling.WebRunner/SequenceDiagram.aspx.cs
@@ -8,22 +8,19 @@ namespace KangaModelling.WebApplication
 {
 	public class SequenceDiagram : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			string codeText = Request["code"] ?? string.Empty;
-
-			var arguments = new DiagramArguments(codeText, DiagramType.Sequence, DiagramStyle.Sketchy);
-			var result = DiagramFactory.Create(arguments);
-			using (result)
-			{
-				Response.Cache.SetCacheability(System.Web.HttpCacheability.Public);
-				Response.ContentType = "image/png";
-
-                MemoryStream temp = new MemoryStream();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string code = Request["code"] ?? string.Empty;
+            var arguments = new DiagramArguments(code, DiagramType.Sequence, DiagramStyle.Sketchy);
+            Response.Cache.SetCacheability(System.Web.HttpCacheability.Public);
+            Response.ContentType = "image/png";
+            using (var result = DiagramFactory.Create(arguments))
+            using (MemoryStream temp = new MemoryStream())
+            {
                 result.Image.Save(temp, ImageFormat.Png);
                 byte[] buffer = temp.GetBuffer();
                 Response.OutputStream.Write(buffer, 0, buffer.Length);
-			}
-		}
+            }
+        }
 	}
 }
