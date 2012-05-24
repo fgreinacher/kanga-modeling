30dfb72a563a104d46aa5834737d7aa1b3d8f2eb
StefanHanke <Stefan.Hanke@gmx.org>
2012-05-24 23:04:14 +0200

    code cleanup on the CD model classes

--- Source/KangaModeling.Compiler/ClassDiagrams/Model/IClass.cs
+++ Source/KangaModeling.Compiler/ClassDiagrams/Model/IClass.cs
@@ -2,48 +2,6 @@
 
 namespace KangaModeling.Compiler.ClassDiagrams.Model
 {
-
-    /// <summary>
-    /// The visilibity of a member
-    /// </summary>
-    public enum VisibilityModifier
-    {
-        Public,
-        Internal,
-        Private,
-        Protected
-    }
-
-    /// <summary>
-    /// Represents one field of an IClass instance.
-    /// </summary>
-    public interface IField
-    {
-        string Name { get; }
-        string Type { get; }
-        VisibilityModifier Visibility { get; }
-    }
-
-    public interface IMethod
-    {
-        string Name { get; }
-        string ReturnType { get; }
-        IEnumerable<MethodParameter> Parameters { get; }
-        VisibilityModifier Visibility { get; }
-    }
-
-    public struct MethodParameter
-    {
-        public MethodParameter(string name, string type) : this()
-        {
-            Name = name;
-            Type = type;
-        }
-
-        public string Name { get; private set; }
-        public string Type { get; private set; }
-    }
-
     /// <summary>
     /// Represents a class in the CD model.
     /// </summary>
