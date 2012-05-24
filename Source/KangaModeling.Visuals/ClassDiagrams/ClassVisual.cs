c440520e8fc650510ea06ea0811bbabd1224db11
StefanHanke <Stefan.Hanke@gmx.org>
2012-05-24 23:52:18 +0200

    implemented methods visualization

--- Source/KangaModeling.Visuals/ClassDiagrams/ClassVisual.cs
+++ Source/KangaModeling.Visuals/ClassDiagrams/ClassVisual.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using KangaModeling.Compiler.ClassDiagrams.Model;
@@ -27,6 +28,7 @@ namespace KangaModeling.Visuals.ClassDiagrams
             if (cd == null) throw new ArgumentNullException("cd");
             _class = cd;
             _fieldInfos = new Dictionary<IField, FieldVisualInfo>();
+            _methodInfos = new Dictionary<IMethod, FieldVisualInfo>();
         }
 
         protected override void DrawCore(IGraphicContext graphicContext)
@@ -37,23 +39,36 @@ namespace KangaModeling.Visuals.ClassDiagrams
             _fieldsCompartment.DrawCore(graphicContext);
             foreach(var fi in _fieldInfos)
                 fi.Value.DrawCore(graphicContext);
+
+            if (_methodInfos.Count > 0)
+                _methodsCompartment.DrawCore(graphicContext);
+            foreach (var mi in _methodInfos)
+                mi.Value.DrawCore(graphicContext);
         }
 
         protected override void LayoutCore(IGraphicContext graphicContext)
         {
             LayoutClassnameCompartment(graphicContext);
             LayoutFieldsCompartment(graphicContext);
+            LayoutMethodsCompartment(graphicContext);
 
-            var totalYSize = _classnameCompartment.Size.Height + _fieldsCompartment.Size.Height;
+            var totalYSize = _classnameCompartment.Size.Height + _fieldsCompartment.Size.Height + _methodsCompartment.Size.Height;
             var maxFieldWidth = Math.Max(_classnameCompartment.Size.Width, _fieldsCompartment.Size.Width);
+            maxFieldWidth = Math.Max(maxFieldWidth, _methodsCompartment.Size.Width);
+
+            // need to set the width on each compartment...
+            _classnameCompartment.Size = new Size(maxFieldWidth, _classnameCompartment.Size.Height);
+            _fieldsCompartment.Size = new Size(maxFieldWidth, _fieldsCompartment.Size.Height);
+            _methodsCompartment.Size = new Size(maxFieldWidth, _methodsCompartment.Size.Height);
 
             Size = new Size(maxFieldWidth, totalYSize);
         }
 
         private void LayoutFieldsCompartment(IGraphicContext graphicContext)
         {
-            float yCursor = _classnameCompartment.Size.Height;
-            float maxFieldWidth = _classnameCompartment.Size.Width;
+            const float topBottomPadding = 5f;
+            float yCursor = _classnameCompartment.Size.Height + topBottomPadding;
+            float maxFieldWidth = 0f;
             foreach (var field in _class.Fields)
             {
                 var text = GenerateFieldText(field);
@@ -67,8 +82,10 @@ namespace KangaModeling.Visuals.ClassDiagrams
 
                 maxFieldWidth = Math.Max(maxFieldWidth, fieldTextSize.Width + 10f);
             }
+            yCursor += topBottomPadding;
 
             // TODO extension method on Size?
+            // TODO setting the classname compartment size HAS NOTHING VERLOREN HIER!
             _classnameCompartment.Size = new Size(maxFieldWidth, _classnameCompartment.Size.Height);
             _fieldsCompartment = new CompartmentVisualInfo {
                 Location = new Point(0f, _classnameCompartment.Size.Height),
@@ -76,33 +93,56 @@ namespace KangaModeling.Visuals.ClassDiagrams
             };
         }
 
-        //private void LayoutMethodsCompartment(IGraphicContext graphicContext)
-        //{
-        //    float yCursor = _methodsCompartment.Size.Height;
-        //    float maxFieldWidth = _methodsCompartment.Size.Width;
-        //    foreach (var method in _class.)
-        //    {
-        //        var text = GenerateFieldText(field);
-        //        var fieldTextSize = graphicContext.MeasureText(text, Font.Handwritten, 12);
-        //        _fieldInfos[field] = new FieldVisualInfo
-        //        {
-        //            TextSize = fieldTextSize,
-        //            FieldText = text,
-        //            TextPosition = new Point(10f, yCursor)
-        //        };
-        //        yCursor += fieldTextSize.Height;
+        private void LayoutMethodsCompartment(IGraphicContext graphicContext)
+        {
+            const float topBottomPadding = 5f;
+            float yCursor = _fieldsCompartment.Size.Height + _fieldsCompartment.Location.Y + topBottomPadding;
+            float maxFieldWidth = 0f;
+            foreach (var method in _class.Methods)
+            {
+                var text = GenerateMethodText(method);
+                var fieldTextSize = graphicContext.MeasureText(text, Font.Handwritten, 12);
+                _methodInfos[method] = new FieldVisualInfo
+                {
+                    TextSize = fieldTextSize,
+                    FieldText = text,
+                    TextPosition = new Point(10f, yCursor)
+                };
+                yCursor += fieldTextSize.Height;
 
-        //        maxFieldWidth = Math.Max(maxFieldWidth, fieldTextSize.Width + 10f);
-        //    }
+                maxFieldWidth = Math.Max(maxFieldWidth, fieldTextSize.Width + 10f);
+            }
+            yCursor += topBottomPadding;
 
-        //    // TODO extension method on Size?
-        //    _classnameCompartment.Size = new Size(maxFieldWidth, _classnameCompartment.Size.Height);
-        //    _fieldsCompartment = new CompartmentVisualInfo
-        //    {
-        //        Location = new Point(0f, _classnameCompartment.Size.Height),
-        //        Size = new Size(maxFieldWidth, yCursor - _classnameCompartment.Size.Height)
-        //    };
-        //}
+            _methodsCompartment = new CompartmentVisualInfo
+            {
+                Location = new Point(0f, _fieldsCompartment.Size.Height + _fieldsCompartment.Location.Y),
+                Size = new Size(maxFieldWidth, yCursor - _fieldsCompartment.Size.Height - _fieldsCompartment.Location.Y)
+            };
+        }
+
+        private string GenerateMethodText(IMethod method)
+        {
+            var methodNameBuilder = new StringBuilder();
+
+            // TODO ToString() on IMethod!
+            methodNameBuilder.Append(Visibility2String(method.Visibility));
+            methodNameBuilder.Append(method.Name);
+            methodNameBuilder.Append("(");
+            foreach(var parameter in method.Parameters)
+            {
+                methodNameBuilder.Append(parameter.Type + " " + parameter.Name);
+                methodNameBuilder.Append(", ");
+            }
+            if(method.Parameters.Any())
+                methodNameBuilder.Remove(methodNameBuilder.Length - 2, 2);
+
+            // TODO return type
+
+            methodNameBuilder.Append(")");
+
+            return methodNameBuilder.ToString();
+        }
 
         private void LayoutClassnameCompartment(IGraphicContext graphicContext)
         {
@@ -127,25 +167,36 @@ namespace KangaModeling.Visuals.ClassDiagrams
             };
         }
 
+        // TODO ToString on IField!
         private static String GenerateFieldText(IField field)
         {
             if (field == null) throw new ArgumentNullException("field");
             var sb = new StringBuilder();
 
-            // TODO mapping from enum to string is multiple defined!
-            switch(field.Visibility)
-            {
-                case VisibilityModifier.Public: sb.Append("+");break;
-                case VisibilityModifier.Protected: sb.Append("#"); break;
-                case VisibilityModifier.Private: sb.Append("~"); break;
-                case VisibilityModifier.Internal: sb.Append("-"); break;
-            }
+            sb.Append(Visibility2String(field.Visibility));
 
             sb.Append(string.Format("{0}", field.Name));
             if(field.Type != null)
                 sb.Append(string.Format(": {0}", field.Type));
 
             return sb.ToString();
+        }
+
+        private static string Visibility2String(VisibilityModifier visibilityModifier)
+        {
+            switch (visibilityModifier)
+            {
+                case VisibilityModifier.Public:
+                    return "+";
+                case VisibilityModifier.Protected:
+                    return "#";
+                case VisibilityModifier.Private:
+                    return "~";
+                case VisibilityModifier.Internal:
+                    return "-";
+            }
+
+            throw new ArgumentException("unexpected visibility modifier: " + visibilityModifier.ToString());
         }
 
         private struct FieldVisualInfo
@@ -202,6 +253,7 @@ namespace KangaModeling.Visuals.ClassDiagrams
         }
 
         private readonly Dictionary<IField, FieldVisualInfo> _fieldInfos;
+        private readonly Dictionary<IMethod, FieldVisualInfo> _methodInfos;
 
     }
 
