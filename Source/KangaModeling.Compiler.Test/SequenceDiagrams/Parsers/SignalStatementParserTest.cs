495ae6041782f855589a5b00134cb14c3694f0f3
FlorianGreinacher <adgrfl0@WIN72057C.ww004.siemens.net>
2012-05-30 13:32:32 +0200

    Trimming participant tokens in signals

--- Source/KangaModeling.Compiler.Test/SequenceDiagrams/Parsers/SignalStatementParserTest.cs
+++ Source/KangaModeling.Compiler.Test/SequenceDiagrams/Parsers/SignalStatementParserTest.cs
@@ -10,7 +10,7 @@ namespace KangaModeling.Compiler.Test.SequenceDiagrams.Parsers
     [TestFixture]
     public class SignalStatementParserTest
     {
-        [TestCase("A->B", new[]{typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(CallSignalStatement)})]
+        [TestCase("A->B", new[] { typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(CallSignalStatement) })]
         [TestCase("A-->B", new[] { typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(ReturnSignalStatement) })]
         [TestCase("A<-B", new[] { typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(CallSignalStatement) })]
         [TestCase("A<--B", new[] { typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(ReturnSignalStatement) })]
@@ -21,7 +21,7 @@ namespace KangaModeling.Compiler.Test.SequenceDiagrams.Parsers
         [TestCase("<--B", new[] { typeof(MissingArgumentStatement), typeof(EnsureParticipantStatement) })]
 
         [TestCase("A->", new[] { typeof(EnsureParticipantStatement), typeof(MissingArgumentStatement) })]
-        [TestCase("A-->", new[] { typeof(EnsureParticipantStatement), typeof(MissingArgumentStatement)})]
+        [TestCase("A-->", new[] { typeof(EnsureParticipantStatement), typeof(MissingArgumentStatement) })]
         [TestCase("A<-", new[] { typeof(EnsureParticipantStatement), typeof(MissingArgumentStatement) })]
         [TestCase("A<--", new[] { typeof(EnsureParticipantStatement), typeof(MissingArgumentStatement) })]
 
@@ -40,36 +40,45 @@ namespace KangaModeling.Compiler.Test.SequenceDiagrams.Parsers
         [TestCase("<-B: text", new[] { typeof(MissingArgumentStatement), typeof(EnsureParticipantStatement) })]
         [TestCase("<--B: text", new[] { typeof(MissingArgumentStatement), typeof(EnsureParticipantStatement) })]
 
-        [TestCase("A<-->B: text", new[] {typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(UnknownStatement)})]
+        [TestCase("A<-->B: text", new[] { typeof(EnsureParticipantStatement), typeof(EnsureParticipantStatement), typeof(UnknownStatement) })]
         [TestCase("skdjskdj: text", new[] { typeof(MissingArgumentStatement), typeof(MissingArgumentStatement) })]
         [TestCase("----->", new[] { typeof(MissingArgumentStatement), typeof(MissingArgumentStatement) })]
         public void ParseTest(string input, Type[] expectedStatementTypes)
         {
             var target = new SignalStatementParser();
-            var actualStatementTypes = target.Parse(input).Select(statement=> statement.GetType());
+            var actualStatementTypes = target.Parse(input).Select(statement => statement.GetType());
             CollectionAssert.AreEquivalent(expectedStatementTypes, actualStatementTypes);
         }
 
-		[TestCase("A->B", "A", "B")]
-		[TestCase("A-->B", "A", "B")]
-		[TestCase("A<-B", "B", "A")]
-		[TestCase("A<--B", "B", "A")]
-		public void VerifySourceAndTarget(string input, string expectedSourceValue, string expectedTargetValue)
-		{
-			var parser = new SignalStatementParser();
-			var statements = parser.Parse(input);
-			var signalStatement = statements.OfType<SignalStatement>().Single();
+        [TestCase("A->B", "A", "B")]
+        [TestCase(" A->B", "A", "B")]
+        [TestCase(" A ->B", "A", "B")]
+        [TestCase(" A->B", "A", "B")]
+        [TestCase("A-> B", "A", "B")]
+        [TestCase("A-> B ", "A", "B")]
+        [TestCase("A->B ", "A", "B")]
 
-			Assert.That(signalStatement.Source.Value, Is.EqualTo(expectedSourceValue));
-			Assert.That(signalStatement.Target.Value, Is.EqualTo(expectedTargetValue));
-		}
+        [TestCase("A-->B", "A", "B")]
+
+        [TestCase("A<-B", "B", "A")]
+
+        [TestCase("A<--B", "B", "A")]
+        public void VerifySourceAndTarget(string input, string expectedSourceValue, string expectedTargetValue)
+        {
+            var parser = new SignalStatementParser();
+            var statements = parser.Parse(input);
+            var signalStatement = statements.OfType<SignalStatement>().Single();
+
+            Assert.That(signalStatement.Source.Value, Is.EqualTo(expectedSourceValue));
+            Assert.That(signalStatement.Target.Value, Is.EqualTo(expectedTargetValue));
+        }
 
         [Ignore]
         public void EnsureTokensSimple(string input, string[] expectedTokenValues)
         {
             var target = new SignalStatementParser();
             var actual = target.Parse(input);
-            var actualTokenvalues = actual.SelectMany(statement=>statement.Tokens()).Select(token => token.Value);
+            var actualTokenvalues = actual.SelectMany(statement => statement.Tokens()).Select(token => token.Value);
             CollectionAssert.AreEquivalent(expectedTokenValues, actualTokenvalues);
         }
     }
