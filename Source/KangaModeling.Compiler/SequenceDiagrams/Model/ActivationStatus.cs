eea3f4b816cec08a23c9d18472ed6f73b7e195d3
FlorianGreinacher <post@floriangreinacher.de>
2012-06-07 22:36:59 +0200

    Deleted obsolete folders & files

--- Source/KangaModeling.Compiler/SequenceDiagrams/Model/ActivationStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KangaModeling.Compiler.SequenceDiagrams
-{
-    public enum ActivationStatus  {
-        /// <summary>The participant is being activated.</summary>
-        Activate,
-        /// <summary>The participant is being deactivated.</summary>
-        Deactivate,
-    }
-}
