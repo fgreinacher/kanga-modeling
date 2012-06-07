d736bb207e9b62f78821f98e6447590147dba101
FlorianGreinacher <post@floriangreinacher.de>
2012-06-07 23:32:32 +0200

    Some restructurings to ApiController

--- Source/KangaModeling.Web.Api/Controllers/ApiController.cs
+++ Source/KangaModeling.Web.Api/Controllers/ApiController.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using KangaModeling.Facade;
 using System.IO;
 using System.Drawing.Imaging;
 using System.Web.Caching;
-using System.Web.UI;
 
 namespace KangaModeling.Web.Controllers
 {
@@ -16,16 +14,15 @@ namespace KangaModeling.Web.Controllers
         private const string c_StyleDefault = c_StyleSketchy;
         private const string c_StyleSketchy = "sketchy";
         private const string c_StyleClassic = "classic";
-
         private const string c_TypeSequence = "sequence";
 
-        private static readonly Dictionary<string, DiagramStyle> s_StyleMappings = new Dictionary<string, DiagramStyle>()
+        private static readonly Dictionary<string, DiagramStyle> s_StyleMappings = new Dictionary<string, DiagramStyle>
         {
             { c_StyleSketchy, DiagramStyle.Sketchy },
             { c_StyleClassic, DiagramStyle.Classic },
         };
 
-        private static readonly Dictionary<string, DiagramType> s_TypeMappings = new Dictionary<string, DiagramType>()
+        private static readonly Dictionary<string, DiagramType> s_TypeMappings = new Dictionary<string, DiagramType>
         {
             { c_TypeSequence, DiagramType.Sequence },
         };
@@ -34,14 +31,19 @@ namespace KangaModeling.Web.Controllers
         // GET: /Api
         public ActionResult Index()
         {
-            return RedirectToAction("help");
+            return new EmptyResult();
         }
 
         //
-        // GET: /Api/Help        
-        public ActionResult Help()
+        // GET: /Api/Js        
+        public ActionResult Js()
         {
-            return View();
+            string jsPath = Server.MapPath("~/Scripts/kanga.js");
+            string js = System.IO.File.ReadAllText(jsPath);
+
+            js = js.Replace("_KANGA_API_BASE_URI_", Url.Action(string.Empty, "api", null, "http"));
+
+            return new JavaScriptResult { Script = js };
         }
 
         //
@@ -49,18 +51,7 @@ namespace KangaModeling.Web.Controllers
         [AllowCrossSiteJson]
         public ActionResult Get(string id)
         {
-            if (string.IsNullOrEmpty(id))
-            {
-                return new HttpStatusCodeResult(400, "Missing parameter 'id'.");
-            }
-
-            var buffer = HttpContext.Cache.Get(id) as byte[];
-            if (buffer == null)
-            {
-                return new HttpStatusCodeResult(400, string.Format("Invalid value '{0}' for parameter 'id'.", id));
-            }
-
-            return new FileStreamResult(new MemoryStream(buffer), "image/png");
+            return CreateImageResult(id);
         }
 
         //
@@ -85,32 +76,14 @@ namespace KangaModeling.Web.Controllers
             var diagramArguments = new DiagramArguments(diagramSource, diagramType, diagramStyle);
             using (var diagramResult = DiagramFactory.Create(diagramArguments))
             {
-                var id = CreateImageId();
+                var id = Guid.NewGuid().ToString();
 
                 SaveImage(diagramResult, id);
 
                 return CreateResult(diagramResult, id);
             }
         }
-
-        //
-        // GET: /Api/Js        
-        [AllowCrossSiteJson]
-        public ActionResult Js()
-        {
-            string jsPath = Server.MapPath("~/Scripts/kanga.js");
-            string js = System.IO.File.ReadAllText(jsPath);
-
-            js = js.Replace("_KANGA_API_BASE_URI_", Url.Action(string.Empty, "api", null, "http"));
-
-            return new JavaScriptResult() { Script = js };
-        }
-
-        private static string CreateImageId()
-        {
-            return Guid.NewGuid().ToString();
-        }
-
+        
         private void SaveImage(DiagramResult result, string imageId)
         {
             using (var imageStream = new MemoryStream())
@@ -130,34 +103,56 @@ namespace KangaModeling.Web.Controllers
 
         private ActionResult CreateResult(DiagramResult result, string id)
         {
-            if (Request.AcceptTypes.Any(acceptType => acceptType.Contains("json")))
+            if (Request.AcceptTypes != null &&
+                Request.AcceptTypes.Any(acceptType => acceptType.Contains("json")))
             {
-                return new JsonResult
-                {
-                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
-                    Data = new
-                    {
-                        source = result.Arguments.Source,
-                        errors = result.Errors.Select(error =>
-                           new
-                           {
-                               message = error.Message,
-                               token = new
-                               {
-                                   line = error.TokenLine,
-                                   start = error.TokenStart,
-                                   end = error.TokenStart + error.TokenLength,
-                                   value = error.TokenValue,
-                               }
-                           }),
-                        diagram = Url.Action("get", "api", new { Id = id }, "http"),
-                        diagramWidth = result.Image.Width,
-                        diagramHeight = result.Image.Height,
-                    }
-                };
+                return CreateJsonResult(result, id);
             }
 
-            return RedirectToAction("get", new { id = id });
+            return CreateImageResult(id);
+        }
+
+        private ActionResult CreateImageResult(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return new HttpStatusCodeResult(400, "Missing parameter 'id'.");
+            }
+
+            var buffer = HttpContext.Cache.Get(id) as byte[];
+            if (buffer == null)
+            {
+                return new HttpStatusCodeResult(400, string.Format("Invalid value '{0}' for parameter 'id'.", id));
+            }
+
+            return new FileStreamResult(new MemoryStream(buffer), "image/png");
+        }
+
+        private ActionResult CreateJsonResult(DiagramResult result, string id)
+        {
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                Data = new
+                {
+                    source = result.Arguments.Source,
+                    errors = result.Errors.Select(error =>
+                       new
+                       {
+                           message = error.Message,
+                           token = new
+                           {
+                               line = error.TokenLine,
+                               start = error.TokenStart,
+                               end = error.TokenStart + error.TokenLength,
+                               value = error.TokenValue,
+                           }
+                       }),
+                    diagram = Url.Action("get", "api", new { Id = id }, "http"),
+                    diagramWidth = result.Image.Width,
+                    diagramHeight = result.Image.Height,
+                }
+            };
         }
     }
 }
